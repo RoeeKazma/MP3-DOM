58624d3e43eb715c50c9eaaf119b21beb1932c75
Roee Kazma <Roeekazma@gmail.com>
2021-09-14 10:28:59 +0300

    Revert "added back my work"
    This reverts commit deec5080d44e5563321522f7b87e729c3b87fb34.

--- scripts/index.js
+++ scripts/index.js
@@ -4,43 +4,18 @@
  *
  * @param {String} songId - the ID of the song to play
  */
- function playSong(songId) {
-    const selectedSong = document.getElementById(songId);
-    const classes = []
-    classes.push(["selected"])
-
-    const songs = document.getElementsByClassName("song");
-    for (let song of songs) {
-        song.classList.remove(classes)
-    }
-    selectedSong.classList.add(classes);
+function playSong(songId) {
+    // Your code here
 }
-
 
 /**
  * Creates a song DOM element based on a song object.
  */
 function createSongElement({ id, title, album, artist, duration, coverArt }) {
     const children = []
-    const ul = document.createElement("ul");
-    for (let i = 0; i < 5; i++)
-    {
-        if (arguments[i] === arguments[4])
-        {
-            arguments[i] = durationConvertor(arguments[4]);
-        }
-        let list = document.createElement("li"); 
-        list.innerText = arguments[i]
-        ul.append(list);
-    }
-    let currentImg= document.createElement("img");
-    currentImg.src= arguments[5];
-    ul.appendChild(currentImg);
-    children.push(ul)
     const classes = []
-    classes.push(["song"]) // CSS later
-    const attrs = { onclick: `playSong(${arguments[0]})`,}
-    return createElement("div", children, classes, attrs, arguments[0])
+    const attrs = { onclick: `playSong(${id})` }
+    return createElement("div", children, classes, attrs)
 }
 
 /**
@@ -48,19 +23,9 @@ function createSongElement({ id, title, album, artist, duration, coverArt }) {
  */
 function createPlaylistElement({ id, name, songs }) {
     const children = []
-
-    const ul= document.createElement("ul");
-    for(let i=0; i<3; i++)
-    {
-        let li= document.createElement("li");
-        li.innerHTML = arguments[i];
-        ul.appendChild(li);
-    }
-    children.push(ul);
     const classes = []
-    classes.push(["playlist"]) // CSS later
     const attrs = {}
-    return createElement("div", children, classes, attrs, id)
+    return createElement("div", children, classes, attrs)
 }
 
 /**
@@ -75,40 +40,8 @@ function createPlaylistElement({ id, name, songs }) {
  * @param {Array} classes - the class list of the new element
  * @param {Object} attributes - the attributes for the new element
  */
-function createElement(tagName, children = [], classes = [], attributes = {}, id) {
-    const element = document.createElement(tagName);
-    for (let child of children)
-    {
-        element.appendChild(child);
-    }
-    for (let cls of classes)
-    {
-        element.classList.add(cls);
-    }
-    
-    Object.entries(attributes).forEach(([key,value]) => {
-        if (key !== undefined) {
-            element.setAttribute(key, value);
-        }
-    })
-
-    element.id = id;
-    return element;
+function createElement(tagName, children = [], classes = [], attributes = {}) {
+    // Your code here
 }
 
-
-
 // You can write more code below this line
-
-sortedSongs();
-
-sortedPlaylists(); 
-
-printAllSongs();
-
-printAllPlaylists();
-
-// const check = document.createElement("h2")
-// check.textContent = "Hello World";
-// const songsElement = document.getElementById("songs");
-// songsElement.appendChild(check)
